483ae8cdf0ab993132c9be989aafa1070a5a57f3
Cory <oneillco117@gmail.com>
2021-11-08 22:54:48 -0500

    committed

--- parrot.rb
+++ parrot.rb
@@ -1,14 +1,3 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-
-# def parrot(speak = "Squawk!", e = "Pretty bird!")
-#    speak
-   
-# end
-
-# parrot("Pretty bird!")
-
-
 def parrot(string="Squawk!")
    puts "#{string}"
    "#{string}"
